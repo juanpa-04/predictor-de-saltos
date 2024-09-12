d59a4e41ad186445f9e54a12c69ef816681b4aa2
DAcosta3103 <diego.acostaobando@ucr.ac.cr>
2024-09-12 05:26:38 -0600

    Se crea el predictor tipo gshare, se renombra el archivo donde están las clases Branch Target Buffer y Pattern History Table. También se añaden algunos comentarios para claridad en los archivos BTBPHT.py y program_counter.py
    Signed-off-by: DAcosta3103 <diego.acostaobando@ucr.ac.cr>

--- branch_predictor/BTBPHT.py
+++ branch_predictor/BTBPHT.py
@@ -1,6 +1,7 @@
 # Clase para el BTB (Branch Target Buffer)
 class BranchTargetBuffer: #almacena direcciones de salto y sus destinos para prever
     #hacia donde saltar en caso que se tome el salto
+
     def __init__(self, size=1024):# Inicializa el tamaño del BTB (por defecto 1024 entradas)
         self.size = size # Crea un diccionario vacío donde se almacenarán las direcciones y sus destinos
         self.buffer = {} #un diccionario "buffer" donde se almacenarán las direcciones de salto como claves y destinos como valores
@@ -10,7 +11,7 @@ class BranchTargetBuffer: #almacena direcciones de salto y sus destinos para pre
         return self.buffer.get(address, None) # usa get, para devolver el destino si la dirección esta en el buffer, sino da "none"
     
     def update(self, address, target):
-        # Si el BTB ha alcanzado su tamaño máximo, elimina la entrada más antigua (primer elemento) y la remplaza con una nueva entrada
+        # Si el BTB ha alcanzado su tamaño máximo, elimina la entrada más antigua (primer elemento) y la remplaza con una nueva entrada, política de reemplazo FIFO
         if len(self.buffer) >= self.size:
             self.buffer.pop(next(iter(self.buffer)))  # Elimina la entrada más antigua
         self.buffer[address] = target # Agrega la nueva dirección con su destino en el buffer
@@ -20,11 +21,13 @@ class BranchTargetBuffer: #almacena direcciones de salto y sus destinos para pre
         for i, (address, target) in enumerate(self.buffer.items()): #itera sobre el "buffer" obteniendo las direcciones y destinos
             if i >= 10: break  # Limita la salida a las primeras 10 entradas, esto por que estamos probando
             print(f"Address: {hex(address)}, Target: {target}") #imprime la dirección en hexadecimal y destino
+            
 # Clase para el PHT (Pattern History Table)
 class PatternHistoryTable: #almacena los salto, saltos tomados o no
     def __init__(self, size=1024): # Inicializa el tamaño de la tabla (por defecto 1024 entradas)
         self.size = size # Crea una tabla con 'size' entradas, todas inicializadas en 0
         self.table = [0] * size #inicia la lista con el numero de entradas que diga "size", todas con valor 0, esto es el historial de predicciones
+        
     def get(self, index):# Devuelve el valor en el índice dado, utilizando el tamaño para asegurar que esté en la tabla
         return self.table[index % self.size] #devuelve el valor en la posición que tiene de parametro
 
